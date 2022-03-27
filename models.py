128fd23bfc332815665146541f9adb3cf73e9005
Seligmann <zachary.a.seligman@gmail.com>
2022-03-27 01:07:06 -0500

    Added has_bio and bio to db to enable more functionality on profile pages

--- models.py
+++ models.py
@@ -7,3 +7,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    has_bio = db.Column(db.Boolean, default=False)
+    bio = db.Column(db.String(5000), nullable=True)
+
+    # day registered, last online, register date, last ip
