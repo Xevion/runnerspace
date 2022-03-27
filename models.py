4c0b3bdbe533447f1d62c7449f971123f63f0a30
Xevion <xevioni@yandex.com>
2022-03-27 02:45:41 -0500

    Extend User model and create Post model

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from flask_login import UserMixin
+from sqlalchemy import func
+
 from .create_app import db
 
 
@@ -7,7 +9,16 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-    has_bio = db.Column(db.Boolean, default=False)
-    bio = db.Column(db.String(5000), nullable=True)
+    about_me = db.Column(db.String(5000), nullable=True)
+    time_registered = db.Column(db.DateTime, nullable=False, server_default=func.now())
+    last_seen = db.Column(db.DateTime, nullable=False, server_default=func.now())
+    last_ip = db.Column(db.String(64), nullable=True)
 
-    # day registered, last online, register date, last ip
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.Integer)
+    text = db.Column(db.Text)
+    date_posted = db.Column(db.DateTime, server_default=func.now())
+    date_updated = db.Column(db.DateTime, nullable=True)
+    likes = db.Column(db.Text, default='[]')
