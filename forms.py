96b4dba73c13e006aec53e6878279a0d5805d54a
Xevion <xevioni@yandex.com>
2022-03-27 11:50:53 -0500

    Add comment posting form

--- forms.py
+++ forms.py
@@ -41,3 +41,21 @@ def new_post():
     db.session.commit()
 
     return redirect(url_for('main.view_post', post_id=post.id))
+
+
+@blueprint.route('/feed/<post_id>/comment', methods=['POST'])
+@login_required
+def add_comment(post_id: int):
+    post = Post.query.get_or_404(post_id)
+
+    comment_text: str = request.form.get('comment-text')
+
+    if len(comment_text) > 50:
+        flash('Cannot have more than 50 characters of text.')
+        return redirect(url_for('main.view_post', post_id=post_id))
+
+    comment = Comment(post=post.id, author=current_user.id, text=comment_text)
+    db.session.add(comment)
+    db.session.commit()
+
+    return redirect(url_for('main.view_post', post_id=post.id))
