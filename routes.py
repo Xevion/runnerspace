2fa03a496b05945d1dd1e1a2d0cf7a8f855ef176
Xevion <xevioni@yandex.com>
2022-03-27 02:02:58 -0500

    Finishing touches on user template page

--- routes.py
+++ routes.py
@@ -1,65 +1,67 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from flask_login import login_required
+
+from .models import User
 
 blueprint = Blueprint('main', __name__)
 
 
-@blueprint.route('/profile')
-@login_required
-def profile():
-    return render_template('layouts/profile.html', name=current_user.name)
-
-
 @blueprint.route('/')
 def index():  # put application's code here
-    return render_template('layouts/index.html', user=user)
+    return render_template('layouts/index.html')
 
 
 @blueprint.route('/about')
 def about():
-    return render_template('pages/about.html', user=user)
+    return render_template('pages/about.html')
 
 
 @blueprint.route('/users')
 def browse():
-    return render_template('pages/browse.html', user=user)
+    return render_template('pages/browse.html')
 
 
 @blueprint.route('/feed')
 def feed():
-    return render_template('pages/feed.html', user=user)
+    return render_template('pages/feed.html')
 
 
 @blueprint.route('/messages')
 def messages():
-    return render_template('pages/messages.html', user=user)
+    return render_template('pages/messages.html')
 
 
 @blueprint.route('/search')
 def search():
-    return render_template('pages/search.html', user=user)
+    return render_template('pages/search.html')
 
 
-@blueprint.route('/user/<username>')
+@blueprint.route('/user/<username>/')
 def user(username: str):
+    user = User.query.filter_by(username=username).first_or_404()
     return render_template('pages/user.html', user=user)
+
+@blueprint.route('/user/<username>/edit')
+@login_required
+def profile():
+    return render_template('layouts/user_edit.html')
 
 
 @blueprint.route('/blogs')
 def blogs():
-    return render_template('pages/blogs.html', user=user)
+    return render_template('pages/blogs.html')
 
 
 @blueprint.route('/groups')
 def groups():
-    return render_template('pages/groups.html', user=user)
+    return render_template('pages/groups.html')
 
 
 @blueprint.route('/login', methods=['GET'])
 def login():
-    return render_template('pages/login.html', user=user)
+    return render_template('pages/login.html')
 
 
 @blueprint.route('/signup', methods=['GET'])
 def signup():
-    return render_template('pages/signup.html', user=user)
+    return render_template('pages/signup.html')
