997b1992ce7c9f8733e26a440dcdfe747797a699
Xevion <xevioni@yandex.com>
2022-03-26 22:38:19 -0500

    Add Pipfile for improved versioning system

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_login import UserMixin
 from .create_app import db
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
